a78e967b3201d15395d35f4d481b2da9df50ca42
Bernard BALLESTA <bernard@ballesta.fr>
2016-09-05 17:07:56 +0200

    Club_id dans joueurs_football (remplaace celui de tb_users) =>OK

--- test/app/Http/Controllers/UserController.php
+++ test/app/Http/Controllers/UserController.php
@@ -132,7 +132,7 @@ class UserController extends Controller
             return View('user.login', $this->data);
 
         }
-    }
+    } 
 
     public function postSignin(Request $request)
     {
@@ -144,7 +144,7 @@ class UserController extends Controller
         if (CNF_RECAPTCHA == 'true') $rules['captcha'] = 'required|captcha';
         $validator = Validator::make(Input::all(), $rules);
         if ($validator->passes())
-        { 
+        {
             $remember = (!is_null($request->get('remember')) ? 'true' : 'false');
 
             if (\Auth::attempt(array('email' => $request->input('email'), 'password' => $request->input('password')), $remember)) {
